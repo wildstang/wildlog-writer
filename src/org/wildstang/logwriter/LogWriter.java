eef11ef89a3b4d27ea768e1d8cd74e859dcbb348
mail929 <liam@liamfruzyna.com>
2015-01-31 19:37:13 -0600

    fixed so it actually should work

--- src/org/wildstang/logwriter/LogWriter.java
+++ src/org/wildstang/logwriter/LogWriter.java
@@ -6,26 +6,60 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-public class LogWriter
+public class LogWriter implements Runnable
 {
-
+	List<Map<String, Object>> maps = new ArrayList<>();
+	
 	public static void main(String[] args) throws IOException, ClassNotFoundException
 	{
-		Socket kkSocket = new Socket("roboRIO-111.local", 1111);
-	    
-	    InputStream yourInputStream = kkSocket.getInputStream();
-	    ObjectInputStream mapInputStream = new ObjectInputStream(yourInputStream);
-	    Map <String, Object> hm = (Map) mapInputStream.readObject();
-	    
-	    mapInputStream.close();
-	    
-		//kkSocket = new Socket("", 1112);
+		new LogWriter();
+	}
+
+	public LogWriter()
+	{
+		Thread t = new Thread();
+		t.start();
+	}
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			for(int i = 0; true; i++)
+			{
+				Socket socket = new Socket("roboRIO-111.local", 1111);
+			    
+			    InputStream yourInputStream = null;
+			    while((yourInputStream = socket.getInputStream()) == null){}
+			    ObjectInputStream mapInputStream = new ObjectInputStream(yourInputStream);
+			    maps.add((Map) mapInputStream.readObject());
+			    mapInputStream.close();
+			    if(i % 10 == 0)
+			    {
+			    	write();
+			    }
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 		
+	}
+	
+	public void write() throws IOException
+	{
 		FileOutputStream fileOut = new FileOutputStream("save.data");
 		ObjectOutputStream out = new ObjectOutputStream(fileOut);
-		out.writeObject(hm);
 		out.close();
 		fileOut.close();
 	}
